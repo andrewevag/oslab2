238d56399d95c927a4ccf96ab5186ce0f6af3813
andrewevag <andrewevag@gmail.com>
2021-12-16 00:43:11 +0200

    fixed format logfile

---
+++ tester.sh
@@ -0,0 +1,18 @@
+
+
+if [$2 = "fork"]
+then
+    make test
+    ./test random
+else
+    if [$2 = "sep"]
+    then 
+        for x in 1 2
+        do
+            ./test
+        done
+        else
+            echo "usage : ./tester.sh -opt where opt is fork or sep"
+    fi
+    
+fi
